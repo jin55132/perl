f61dcf06d62a6cd8fe1f145b9d8f7a31ac6b510b
operator1732 <operator1732@LeChuck.(none)>
2013-03-20 01:19:53 +0900

    regex

--- ch7.pl
+++ ch7.pl
@@ -11,11 +11,61 @@ use 5.010;
 		
 # }
 
-while(<STDIN>)
+
+# while(<STDIN>)
+# {
+# 	chomp;
+# 	if(/(Fred|Alfred)/){
+# 		say;
+# 	}
+		
+# }
+
+
+if(! open INPUT, '<', 'input.txt')
+{
+	die "die!";
+}
+
+while(<INPUT>)
 {
 	chomp;
-	if(/(Fred|Alfred)/){
+	if(/\./) # dot is meta character(one letter) requiring back slash
+	{
 		say;
 	}
+}
+
+close INPUT;
+
+
+# while(<STDIN>)
+# {
+# 	if( /[A-Z][a-z]+/ ) {
+# 		print;
+# 	}
 		
+# }
+
+# while(<STDIN>)
+# {
+# 	#if( /(.)\1/ ) { # dot is a wildcard except a newline \n
+# 	if( /(\S)\1/ ) { # \S is non whitespace character
+# 		print;
+# 	}
+# }
+
+while(<STDIN>)
+{
+	#if( /(((wilma)[\w\v\h]*(fred))|((fred)[\w\v\h]*(wilma)))/ ) {
+	#if( /wilma.*fred|fred.*wilma/) {
+
+		if(/wilma/) {
+			if(/fred/) {
+				print;
+			}
+		}
+
+	#	print;
+	#}
 }
