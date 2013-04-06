2a0d5c6de4b00ec6252c894b0b9bbcb08c5ff175
Jin. K <operator1732@gmail.com>
2013-04-06 13:16:21 +0900

    ]

--- processing.pl
+++ processing.pl
@@ -40,16 +40,16 @@ use 5.014;
 
 $_ = <STDIN>;
 
-my $OE = chr( 0xBC ); # get exactly what we intend
+# my $OE = chr( 0xBC ); # get exactly what we intend
 
-if (/$OE/li) { # that's better
-print "Found $OE\n";
-}
-
-# $_ = <STDIN>;
-# if (/OE/ui) { # now uses Unicode
-# print "Found OE\n";
+# if (/$OE/li) { # that's better ( l modifier use locale's rules)
+# print "Found $OE\n";
 # }
+
+$_ = <STDIN>;
+if (/OE/ui) { # now uses Unicode
+print "Found OE\n";
+}
 
 
 
@@ -66,3 +66,50 @@ while (<STDIN>) {
 	chomp;
 	print "$_\n" if /\.png\z/;
 }
+
+$_ = "manfred mann";
+
+if(/^fred/)
+{
+	say "문자열의 첫부분";
+}
+
+$_ = "knute rockne";
+
+if( /rock$/)
+{
+	say "문자열의 끝부분";
+}
+
+$_ = "frederic";
+
+if( /\bfred\b/ )
+{
+	say "단어 경계";
+}
+
+$_ = "searching";
+
+if( /\bsearch\B/ )
+{
+	say "단어 경계 반대";
+}
+
+my $like_per = <STDIN> =~ /\byte\b/i; # =~ regex 연산자
+
+
+my $what = "larry";
+#my $what = shift @ARGV;
+
+while(<>) # read from @ARGV
+{
+	if(/^($what)/)
+	{
+		print "We saw $what in the beginning of $_";
+	}
+
+}
+
+
+
+
