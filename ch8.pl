c3aa3ed4e878db9c6df9234c7008e0605f320e63
Jin. K <operator1732@gmail.com>
2013-04-08 10:27:17 +0900

    exercise

--- ch8.pl
+++ ch8.pl
@@ -7,13 +7,13 @@ while(<>) # 커맨드 arg로 오면 파일을 열고 안오면 표준 입
 {
 	chomp;
 	#2 if(/a\b/) #단어 경계
-	 if(/(a\b)/) #단어 경계
+	 if(/(?<word>\b\w*a\b)(.{0,5})/) #단어 경계
 	{
-		say "'$1'";
-		say "Matched: |$`<$&>$'|\n";
+		say "print $+{word}";
+		say "Matched: |$`<$&>$'|";
 	}
 	else
 	{
-		say "No match: |$_|\n";
+		say "No match: |$_|";
 	}
 }
